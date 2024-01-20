4c1ee40e71104e947b2e47675c3d5dd22a6053b2
Yang Wong <yang wang>
2024-01-20 15:08:51 +0100

    config caffeine

--- src/main/java/org/wy/online_reading/OnlineReadingApplication.java
+++ src/main/java/org/wy/online_reading/OnlineReadingApplication.java
@@ -1,18 +1,44 @@
 package org.wy.online_reading;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Map;
+
 
 /**
  * @author 89589
  */
 @SpringBootApplication
 @MapperScan("org.wy.online_reading.dao.mapper")
+@EnableCaching
+@EnableScheduling
+@Slf4j
 public class OnlineReadingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineReadingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		return args -> {
+			Map<String, CacheManager> beans = context.getBeansOfType(CacheManager.class);
+			log.info("The following cache manager is loaded：");
+			beans.forEach((k,v)->{
+				log.info("{}:{}",k,v.getClass().getName());
+				log.info("Cache：{}",v.getCacheNames());
+			});
+
+		};
 	}
 
 }
---
+++ src/main/java/org/wy/online_reading/core/config/CacheConfig.java
@@ -0,0 +1,41 @@
+package org.wy.online_reading.core.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.wy.online_reading.core.comme.constant.CacheConsts;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 89589
+ */
+public class CacheConfig {
+
+    /**
+     * Caffeine cache manager
+     */
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = new ArrayList<>(CacheConsts.CacheEnum.values().length);
+        for (CacheConsts.CacheEnum c : CacheConsts.CacheEnum.values()) {
+            if (c.isLocal()) {
+                Caffeine<Object, Object> caffeine = Caffeine.newBuilder().recordStats().maximumSize(c.getMaxSize());
+                if (c.getTtl() > 0) {
+                    caffeine.expireAfterWrite(Duration.ofSeconds(c.getTtl()));
+                }
+                caches.add(new CaffeineCache(c.getName(), caffeine.build()));
+            }
+        }
+
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+
+}
